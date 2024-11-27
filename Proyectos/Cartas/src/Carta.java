b38502ad1dd88441a4fe667ca97d194acb151971
Patiño Vera César David <cesardavidpatinovera@gmail.com>
2024-11-26 23:06:37 -0300

    validaciones añadidas para generar valorea y palos aleatorios sin duplicacion

--- Proyectos/Cartas/src/Carta.java
+++ Proyectos/Cartas/src/Carta.java
@@ -1,22 +1,40 @@
+import java.util.*;
+import java.util.Random;
 public class Carta {
+    //Definimos como constantes los valores y palos posibles para una carta
+    private static final Set<String> VALORES_POSIBLES = Set.of("A", "K", "Q", "J", "T", "2", "3", "4", "5", "6", "7", "8", "9");
+    private static final Set<String> PALOS_POSIBLES = Set.of("S", "H", "D", "C");
+
+    //Definimos en un set las cartas creadas
+    private static final Set<String> CARTAS_CREADAS = new HashSet();
+
     public String valor;
     public String palo;
-    public Carta()
-    {
 
+    //constructor aleatorio
+    public Carta () {
+        Random random = new Random();
+        while (true) {
+            String valorRandom = getRandomElemento(VALORES_POSIBLES, random);
+            String paloRandom = getRandomElemento(PALOS_POSIBLES, random);
+            String cartaCompleta = valorRandom + paloRandom;
+
+            if (!CARTAS_CREADAS.contains(cartaCompleta)) {
+                this.valor = valorRandom;
+                this.palo = paloRandom;
+                CARTAS_CREADAS.add(cartaCompleta);
+                break;
+            }
+        }
     }
 
-    public Carta(String completo)
-    {
-        this.valor = String.valueOf(completo.charAt(0)).toUpperCase();
-        this.palo = String.valueOf(completo.charAt(1)).toUpperCase();
+    public static String getRandomElemento(Set<String> set, Random random) {
+        int index = random.nextInt(set.size());
+        return set.stream().skip(index).findFirst().orElse(null);
     }
 
-    String valorPalo()
-    {
+    public String getCarta () {
         return this.valor + this.palo;
     }
-
-
 }
 
--- Proyectos/Cartas/src/Main.java
+++ Proyectos/Cartas/src/Main.java
@@ -3,11 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Carta> cartas = new ArrayList<>();
-        cartas.add(new Carta("1H"));
-        cartas.add(new Carta("2D"));
-        cartas.add(new Carta("3C"));
-        cartas.add(new Carta("2S"));
-        cartas.add(new Carta("1S"));
+
+        Carta carta = new Carta();
+        Carta carta1 = new Carta();
+        Carta carta2 = new Carta();
+        Carta carta3 = new Carta();
+        Carta carta4 = new Carta();
+        System.out.println(carta.getCarta());
+        System.out.println(carta2.getCarta());
+        System.out.println(carta3.getCarta());
+        System.out.println(carta4.getCarta());
+        System.out.println(carta1.getCarta());
 
         String jugadaDevuelta = JuegoPoker.determinarJugada(cartas);
         System.out.println("La jugada es: " + jugadaDevuelta);
